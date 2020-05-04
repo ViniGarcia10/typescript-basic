a3b5f9feb41868a5a37442dcd37a84eb18789f8a
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-05-04 07:49:48 -0400

    typing objects and vectors

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
+import createUser from "./service/CreateUser";
 
 export function helloWorld(req: Request, res: Response) {
+  const user = createUser({
+    name: "vinicius",
+    email: "vinicius@gmail.com",
+    password: "123456",
+    techs: ["react", "react native", { title: "Javascript", experience: 100 }],
+  });
+
   return res.json({ message: "Hello World" });
 }
